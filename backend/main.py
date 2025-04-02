5336fc01ac98d2a6e4e7d4aa1eb1477aa7941d61
Feroz Chand <ferozchand06@gmail.com>
2025-04-03 03:07:27 +0530

    database initialized

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,15 @@
+import os
+import mysql.connector as mysql
+from dotenv import load_dotenv
 from fastapi import FastAPI
-
+load_dotenv()
 app = FastAPI(title="PostOrbit")
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
 
-@app.get("/")
-async def home():
-    return "works!"
-
+if(db_name):
+    print("database exists")
+else:
+    print("database doesnt exists")
