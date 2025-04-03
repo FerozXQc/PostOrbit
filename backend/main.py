9c1dbabd90cbf335b98a5fb47c5be2849d7ab13c
Feroz Chand <ferozchand06@gmail.com>
2025-04-03 22:16:47 +0530

    added prerequisites.txt and initialized jwt auth for FastAPI

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,10 @@
 # import mysql.connector as mysql
 # from dotenv import load_dotenv
 
-from fastapi import FastAPI
-from model import PostSchema
-# load_dotenv()
-
+from fastapi import FastAPI,Body, Depends
+from model import PostSchema,UserSchema,UserLoginSchema
+from jwt_handler import signJWT
+from jwt_bearer import jwtBearer
 app = FastAPI(title="PostOrbit")
 
 # db_host = os.getenv("DB_HOST")
@@ -14,19 +14,19 @@ app = FastAPI(title="PostOrbit")
 # db_name = os.getenv("DB_NAME")
 
 Posts = [
-    {
-        "id":1,
-        "name":"feroz",
-        "desc":"k3tamine super CEO"
-    },
-    {
-        "id":2,
-        "name":"taha",
-        "desc":"deactivated"
-    }
-]
+     {
+         "id":1,
+         "name":"feroz",
+         "desc":"k3tamine super CEO"
+     },
+     {
+         "id":2,
+         "name":"taha",
+         "desc":"deactivated"
+     }
+     ]
 
-@app.get("/")
+@app.get("/",tags=["greet"])
 def greet():
     return "hi"
 
@@ -36,14 +36,37 @@ def posts():
 
 @app.get("/posts/{id}")
 def post(id:int):
-    if id>len(Posts):
-        return "not found"
-    for i in Posts: 
-        if id == i["id"]:
-            return i
+     if id>len(Posts):
+         return "not found"
+     for i in Posts: 
+         if id == i["id"]:
+             return i
         
-@app.post("/addpost")
+@app.post("/addpost", dependencies=[Depends(jwtBearer())])
 def addpost(post:PostSchema):
-    post.id = len(Posts) + 1
-    Posts.append(post.dict())
-    return f"{post} added successfully"
+     post.id = len(Posts) + 1
+     Posts.append(post.dict())
+     return f"{post} added successfully"
+
+users=[]
+
+@app.post("/register")
+def Register(schema:UserSchema =Body()):
+    users.append(schema)
+    return f"{schema.name} added successfully."
+
+def check_user(schema:UserLoginSchema =Body()):
+    for user in users:
+        if user.email == schema.email and user.password == schema.password:
+            return True
+        else:
+            return False
+
+@app.post("/login", tags=["user"])
+def user_login(user:UserLoginSchema = Body(default=None)):
+    if check_user(user):
+        return signJWT(user.email)
+    else:
+        return {
+            "error":"inavlid creds."
+        }
